25cd793ae3de8281b76a82277de745cbae9ebe6e
Harley Dickson <job_2614@hotmail.com>
2019-05-14 19:01:39 -0400

    Icon only next and prev.

--- app/src/main/java/com/bignerdranch/androidprogramming/GeoQuiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/androidprogramming/GeoQuiz/QuizActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -11,8 +12,8 @@ public class QuizActivity extends AppCompatActivity {
 
     private Button mTrueButton;
     private Button mFalseButton;
-    private Button mPrevButton;
-    private Button mNextButton;
+    private ImageButton mPrevButton;
+    private ImageButton mNextButton;
     private TextView mQuestionTextView;
 
     private Question[] mQuestionBank = new Question[] {
@@ -40,14 +41,14 @@ public class QuizActivity extends AppCompatActivity {
         });
 
 
-        mNextButton = (Button) findViewById(R.id.next_button);
+        mNextButton = (ImageButton) findViewById(R.id.next_button);
         mNextButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 nextQuestion();
             }
         });
 
-        mPrevButton = (Button) findViewById(R.id.prev_button);
+        mPrevButton = (ImageButton) findViewById(R.id.prev_button);
         mPrevButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 prevQuestion();
